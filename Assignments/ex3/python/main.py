777d68dea288129edfb0b3cfa05d6917a54695bc
Dino School <dinossht@stud.ntnu.no>
2020-01-30 13:32:02 +0100

    ex3 task 2 a) done

--- Assignments/ex3/python/main.py
+++ Assignments/ex3/python/main.py
@@ -2,20 +2,45 @@ import matplotlib.pyplot as plt
 import numpy as np
 from common import draw_frame
 
+
+# Every equations that are referenced in this file is based on:
+# "Multiple View Geometry in Computer Vision - 2nd edition"
+
+
 def estimate_H(xy, XY):
     #
     # Task 2: Implement estimate_H
     #
-    H = np.eye(3) # Placeholder code
+    A = np.zeros((xy.shape[0]*xy.shape[1], 9))
+    # Based on Algorithm 4.1
+    for r in range(xy.shape[0]):
+        # Equation 14 from assignment
+        x = xy[r, 0]
+        y = xy[r, 1]
+        X = XY[r, 0]
+        Y = XY[r, 1]
+        A[r*2, :] = np.array([X, Y, 1, 0, 0, 0, -X*x, -Y*x, -x])
+        A[r*2+1, :] = np.array([0, 0, 0, X, Y, 1, -X*y, -Y*y, -y])
+
+    # Obtain the SVD of A (section A4.4(p585)). The unit singular vector 
+    # corresponding to the smallest singular value is the solution h. 
+    # Specifically, if A = UDV T with D diagonal with positive diagonal 
+    # entries, arranged in descending order down the diagonal, then h
+    # is the last column of V.
+    _, _, V = np.linalg.svd(A)
+    # Equation 4.2 from textbook
+    H = V[-1, :].reshape(3, 3)
     return H
+
 
 def decompose_H(H):
     #
     # Task 3a: Implement decompose_H
     #
-    T1 = np.eye(4) # Placeholder code
-    T2 = np.eye(4) # Placeholder code
+    T1 = np.eye(4)  # Placeholder code
+    T2 = np.eye(4)  # Placeholder code
     return T1, T2
+
 
 def choose_solution(T1, T2):
     #
@@ -23,34 +48,37 @@ def choose_solution(T1, T2):
     #
     return T1
 
-K           = np.loadtxt('../data/cameraK.txt')
+
+K = np.loadtxt('../data/cameraK.txt')
 all_markers = np.loadtxt('../data/markers.txt')
-XY          = np.loadtxt('../data/model.txt')
-n           = len(XY)
+XY = np.loadtxt('../data/model.txt')
+n = len(XY)
 
 for image_number in range(23):
     I = plt.imread('../data/video%04d.jpg' % image_number)
-    markers = all_markers[image_number,:]
+    markers = all_markers[image_number, :]
     markers = np.reshape(markers, [n, 3])
-    matched = markers[:,0].astype(bool) # First column is 1 if marker was detected
-    uv = markers[matched, 1:3] # Get markers for which matched = 1
+    # First column is 1 if marker was detected
+    matched = markers[:, 0].astype(bool)
+    uv = markers[matched, 1:3]  # Get markers for which matched = 1
 
     # Convert pixel coordinates to normalized image coordinates
-    xy = (uv - K[0:2,2])/np.array([K[0,0], K[1,1]])
+    xy = (uv - K[0:2, 2])/np.array([K[0, 0], K[1, 1]])
 
     H = estimate_H(xy, XY[matched, :2])
-    T1,T2 = decompose_H(H)
+    T1, T2 = decompose_H(H)
     T = choose_solution(T1, T2)
 
     # Compute predicted corner locations using model and homography
     uv_hat = (K@H@XY.T)
-    uv_hat = (uv_hat/uv_hat[2,:]).T
+    uv_hat = (uv_hat/uv_hat[2, :]).T
 
     plt.clf()
     plt.imshow(I, interpolation='bilinear')
     draw_frame(K, T, scale=7)
-    plt.scatter(uv[:,0], uv[:,1], color='red', label='Observed')
-    plt.scatter(uv_hat[:,0], uv_hat[:,1], marker='+', color='yellow', label='Predicted')
+    plt.scatter(uv[:, 0], uv[:, 1], color='red', label='Observed')
+    plt.scatter(uv_hat[:, 0], uv_hat[:, 1], marker='+',
+                color='yellow', label='Predicted')
     plt.legend()
     plt.xlim([0, I.shape[1]])
     plt.ylim([I.shape[0], 0])
