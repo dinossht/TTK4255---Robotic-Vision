c9edf1865bba557d6d2737418039360f522a8e2b
Dino School <dinossht@stud.ntnu.no>
2020-03-05 14:05:46 +0100

    add: task2a

--- Assignments/ex6/python/eight_point.py
+++ Assignments/ex6/python/eight_point.py
@@ -18,8 +18,37 @@ def eight_point(uv1, uv2):
     See HZ Ch. 11.2: The normalized 8-point algorithm (p.281).
     """
 
-    # todo: Compute F
-    F = np.zeros((3,3))
+    # Normalization
+    uv1, T1 = normalize_points(uv1)
+    uv2, T2 = normalize_points(uv2)
+
+    # Find fundemental matrix F_hat' 
+    # (a) Linear solution:
+    A = np.column_stack((
+        uv2[:, 0] * uv1[:, 0], 
+        uv2[:, 0] * uv1[:, 1], 
+        uv2[:, 0], 
+        uv2[:, 1] * uv1[:, 0],
+        uv2[:, 1] * uv1[:, 1],
+        uv2[:, 1],
+        uv1[:, 0],
+        uv1[:, 1],
+        np.ones((len(uv1), 1)) 
+    ))
+
+    U, S, VT = np.linalg.svd(A)
+    F_hat = np.reshape(VT.T[:, -1], (3, 3))
+
+    """
+    # (b) Constraint enforcement:
+    U, S, VT = np.linalg.svd(F_hat)
+    S = np.diag(S)
+    S[-1, -1] = 0
+    F_hat_marked = U@S@VT
+    """
+    
+    # Denomalization
+    F = T2.T@F_hat@T1
     return F
 
 def closest_fundamental_matrix(F):
