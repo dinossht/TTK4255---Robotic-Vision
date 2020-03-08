5169cccd17cfc2fccce5fea575872b34ae01f651
Dino School <dinossht@stud.ntnu.no>
2020-03-08 13:46:55 +0100

    add: task 3d)

--- Assignments/ex6/python/choose_solution.py
+++ Assignments/ex6/python/choose_solution.py
@@ -8,7 +8,19 @@ def choose_solution(uv1, uv2, K1, K2, Rts):
     the one which gives the most points in front of both cameras.
     """
 
-    # todo: Choose the correct solution
-    soln = 0
+    positive_Z_count = np.zeros((4, 1))
+    # Iterate over all points and all possible R,t combinations and 
+    # count which R,t combination gives most positive Z-points
+    for i in range(len(Rts)):
+        for j in range(len(uv1)):
+            R, t = Rts[i]
+            P1, P2 = camera_matrices(K1, K2, R, t) 
+            X = linear_triangulation(uv1[j], uv2[j], P1, P2)
+            
+            # Count how many points have positive Z coordinate
+            if X[-1] > 0:
+                positive_Z_count[i] += 1      
+
+    soln = positive_Z_count.argmax()
     print('Choosing solution %d' % soln)
     return Rts[soln]
