9720930b2cd33fef0f380541f1d6cc3b0932b8db
Dino School <dinossht@stud.ntnu.no>
2020-03-08 17:43:15 +0100

    add: task 4a)

--- Assignments/ex6/python/epipolar_match.py
+++ Assignments/ex6/python/epipolar_match.py
@@ -23,6 +23,46 @@ def epipolar_match(I1, I2, F, uv1):
     # - Use I[v-w : v+w+1, u-w : u+w+1] to extract a window of half-width w around (v,u).
     # - Use the np.sum function.
 
-    # todo: compute uv2
+    # Start iterasjon over alle punkter i uv1
+    # for hvert punkt: regn ut linje i andre bilde
+    # for hvert punkt nedover den linjen: regn ut hvor "likt" det er rundt punktet på linjen med punktet i det andre bildet (husk, et punkt er blitt en linje)
+    # velge punkt i andre bilde som "ligner" mest på punkt i første bilde, putt inn i matrise som er like stor som uv1
+
+    n = uv1.shape[0]
+    H, W = I1.shape
+    w = 10  # image comparison image size
+    
     uv2 = np.zeros(uv1.shape)
+    # Calculate epipolar line in image 2 for a given point in image 1 
+    l = (F @ np.block([uv1, np.ones((n, 1))]).T).T
+    
+    best_SAD = np.inf
+    
+    for i in range(len(uv1)):
+        for u in range(w, H):
+            v = int((1.0 / l[i][1]) * (-l[i][2] - u * l[i][0]))
+            
+            # Window start-end indices
+            u2_start    = u - w
+            u2_end      = u + w + 1
+            v2_start    = v - w
+            v2_end      = v + w + 1
+            u1_start    = int(uv1[i, 0] - w)
+            u1_end      = int(uv1[i, 0] + w + 1)
+            v1_start    = int(uv1[i, 1] - w)
+            v1_end      = int(uv1[i, 1] + w + 1)
+
+            # Check if indices are inside the image
+            if (u2_start >= 0 and u2_end < W and v2_start >= 0 and v2_end < H \
+                and u1_start >= 0 and u1_end < W and v1_start >= 0 and v1_end < H):
+                I_ref = I1[v1_start:v1_end, u1_start:u1_end]
+                I_comp = I2[v2_start:v2_end, u2_start:u2_end]
+
+                # Calculate best sum of absolute intensity difference (SAD)
+                current_SAD = np.sum((I_ref - I_comp)**2)
+                if best_SAD >= current_SAD:
+                    best_SAD = current_SAD
+                    uv2[i, 0] = u
+                    uv2[i, 1] = v
+
     return uv2
